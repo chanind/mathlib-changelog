f2bcdeb3891301957665e9b9d2d700a5d1bb1bca
David Chanin <chanindav@gmail.com>
2022-07-11 14:18:34 +0100

    dedupe commits in item view

--- website/components/ItemChangeHistory.tsx
+++ website/components/ItemChangeHistory.tsx
@@ -4,6 +4,7 @@ import { ChangelogItemData } from "../data/extractDataFromChangelog";
 import { PlusIcon, MinusIcon, CodeIcon } from "@heroicons/react/solid";
 import { ChangeType } from "../data/types";
 import formatTimestamp from "../util/formatTimestamp";
+import { uniqBy } from "lodash";
 
 export interface ChangeHistoryProps {
   item: ChangelogItemData;
@@ -34,22 +35,24 @@ const getLabel = (changeType: ChangeType) => {
 export const ItemChangeHistory: FC<ChangeHistoryProps> = ({ item }) => (
   <div>
     <div className="py-2 flex flex-col">
-      {item.history.map((event, i) => (
-        <Link href={`/commit/${event.commitSha}`} key={i}>
-          <a className="p-2 border border-gray-200 my-1 rounded-md hover:border-gray-400 transition text-gray-800">
-            <div className="pb-1">
-              <span className="text-blue-600">
-                {formatTimestamp(event.commitTimestamp)}
-              </span>
-            </div>
-            <div className="text-gray-400 pb-2 text-xs">{event.diffPath}</div>
-            <div className="text-gray-800 text-sm pl-2 border-l border-right italic mb-2 truncate">
-              {event.commitHeadline}
-            </div>
-            {getLabel(event.type)} {item.name}
-          </a>
-        </Link>
-      ))}
+      {uniqBy(item.history, (event) => `${event.commitSha}-${event.type}`).map(
+        (event, i) => (
+          <Link href={`/commit/${event.commitSha}`} key={i}>
+            <a className="p-2 border border-gray-200 my-1 rounded-md hover:border-gray-400 transition text-gray-800">
+              <div className="pb-1">
+                <span className="text-blue-600">
+                  {formatTimestamp(event.commitTimestamp)}
+                </span>
+              </div>
+              <div className="text-gray-400 pb-2 text-xs">{event.diffPath}</div>
+              <div className="text-gray-800 text-sm pl-2 border-l border-right italic mb-2 truncate">
+                {event.commitHeadline}
+              </div>
+              {getLabel(event.type)} {item.name}
+            </a>
+          </Link>
+        )
+      )}
     </div>
   </div>
 );
